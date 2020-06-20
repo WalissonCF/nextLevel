5b05bf4b479f7daca12dcd48cd8eac72702b2cd1
Walisson <walissoncf44@gmail.com>
2020-06-19 21:20:39 -0300

    Implementation nunjucks

--- next_level/nlw/src/server.js
+++ next_level/nlw/src/server.js
@@ -6,18 +6,29 @@ const server = express()
 server.use(express.static("public"))
 
 // Using template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
 
 // Configuring application paths 
 // Home page
 // Req: request
 // Res: answer
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Titulo"})
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-result.html")
 })
 
 // Connecting server
 server.listen(3000)
+
+// 01:11:35
